4b165ccb4607a1c9f6b5252a7c91529e76a408a3
Miguel Akira <miguel.akira@gmail.com>
2011-09-02 02:10:05 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huddle::Application.config.secret_token = '5a2f5763c2e8ea0a26a442abaef71fee3529e041d3ac32fbeb364744e999706b1a5e6542106c2f5fe8c947f8da1333016983ca87af97ab1096cb7e68dfce8237'
---
+++ test/functional/status_reports_controller_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class StatusReportsControllerTest < ActionController::TestCase
+  setup do
+    @status_report = status_reports(:one)
+  end
+
+  test "creation of status report with data" do 
+    assert_difference('StatusReport.count', 1) do
+      post :create, :status_report => {
+        :project_id =>  projects(:one).to_param
+        :user_id    =>  users(:one).to_param
+        :yesterday  =>  "I did stuff"
+        :today      =>  "I'll do stuff"
+      }
+    end
+    actual = assigns(:status_report)
+    assert_equal(projects(:one).id, actual.project.id)
+    assert_equal(users(:one), actual.user.id)
+    assert_equal(Date.today.to_s(:db), actual.status_date.to_s(:db))
+    assert_redirected_to status_report_path(actual)
+  end
+
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:status_reports)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create status_report" do
+    assert_difference('StatusReport.count') do
+      post :create, :status_report => @status_report.attributes
+    end
+
+    assert_redirected_to status_report_path(assigns(:status_report))
+  end
+
+  test "should show status_report" do
+    get :show, :id => @status_report.to_param
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, :id => @status_report.to_param
+    assert_response :success
+  end
+
+  test "should update status_report" do
+    put :update, :id => @status_report.to_param, :status_report => @status_report.attributes
+    assert_redirected_to status_report_path(assigns(:status_report))
+  end
+
+  test "should destroy status_report" do
+    assert_difference('StatusReport.count', -1) do
+      delete :destroy, :id => @status_report.to_param
+    end
+
+    assert_redirected_to status_reports_path
+  end
+end
